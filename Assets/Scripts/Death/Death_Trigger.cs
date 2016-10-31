6a150f8a096f1b7a2049ebf7083916349049682f
DBeumans <buncho.gamingnetwork@gmail.com>
2016-10-31 15:02:41 +0100

    Added Death Trigger, Random Object Spawner and Object Movement.

---
+++ Assets/Scripts/Death/Death_Trigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death_Trigger : MonoBehaviour {
+
+    string ThisObjectName;
+
+    void Start()
+    {
+        ThisObjectName = this.gameObject.name;
+        Debug.Log("Death Trigger Name: " + ThisObjectName);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(ThisObjectName != other.gameObject.name)
+        {
+            Debug.Log("Object Destroyed! " + other.gameObject.name);
+            Destroy(other.transform.parent.gameObject); // Destroys the whole object.
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Objects_Spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objects_Spawner : SpawningBehaviour {
+
+    //Object to spawn , its a array so you can spawn different objects of the same type.
+    [SerializeField]
+    GameObject[] Objects;
+    //Spawnpoints, where the objects needs to spawn.
+    [SerializeField]
+    Transform spawnPoints;
+    public bool _spawnable = false;
+    [SerializeField]
+    Transform[] spawnPointOneTime;
+    float spawnTime = 2f;
+
+    void Start()
+    {
+        SpawnOnce();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void SpawnOnce()
+    {
+        for (int i = 0; i < spawnPointOneTime.Length; i++)
+        {
+            ObjectSpawner(Objects, spawnPointOneTime[i]);
+        }
+    }
+
+    void Spawn()
+    {
+        _spawnable = false;
+        Debug.Log("Spawned a object");
+        ObjectSpawner(Objects, spawnPoints);
+    }
+}
