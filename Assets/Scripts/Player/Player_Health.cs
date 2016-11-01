f43cff6a7bff2bc6959c2bf6d70cf9e27d7d5db4
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-01 10:06:16 +0100

    Added main menu and player health

---
+++ Assets/Scripts/Player/Player_Health.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Health : MonoBehaviour {
+
+    int _player_health;
+
+    public int PlayerHealth
+    {
+        get
+        {
+            return _player_health;
+        }
+    }
+
+    void Start()
+    {
+        _player_health = 100;
+    }
+
+    void Update()
+    {
+        if(_player_health <= 0)
+        {
+            // player is dead.
+            // do logic.
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag.Contains("Enemy_") || other.gameObject.tag == "Death_Trigger")
+        {
+            SetPlayerDamage(other.gameObject.tag);
+        }
+    }
+
+    void SetPlayerDamage(string TypeOfEnemy)
+    {
+        switch(TypeOfEnemy)
+        {
+            case "Enemy_Spikes": // needs to change to Enemy_<what the object is> example: Enemy_Spikes.
+                _player_health -= 10;
+                break;
+
+            case "Enemy_Stone": // needs to change to Enemy_<what the object is> example: Enemy_Spikes.
+                _player_health -= 15;
+                break;
+
+            case "Enemy_Rocket": // needs to change to Enemy_<what the object is> example: Enemy_Spikes.
+                _player_health -= 20;
+                break;
+
+            case "Death_Trigger": // when it touches the death trigger.
+                _player_health -= 100;
+                break;
+        }
+    }
+}
