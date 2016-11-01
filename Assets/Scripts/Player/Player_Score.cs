3c6a5568f8cf0d1cd7d607eddab780cb99b1ee5f
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-01 08:52:47 +0100

    Changed things in player score script.

--- Assets/Scripts/Player/Player_Score.cs
+++ Assets/Scripts/Player/Player_Score.cs
@@ -3,23 +3,39 @@ using System.Collections;
 
 public class Player_Score : MonoBehaviour {
 
-    int player_score; // private variable, do not change this to public, use the other player score variable to use it in other scripts.
+    int _player_score; // private variable, do not change this to public, use the other player score variable to use it in other scripts.
 
     // use PlayerScore to use it for other scripts , example UI, save scripts.
     public int PlayerScore // Read only.
     {
         get
         {
-            return player_score;
+            return _player_score;
         }
     }
 
     void OnTriggerEnter2D (Collider2D other)
     {
-        string tag = "SpecialObject"; // Change this to the tag the player needs to find to  obtain score.
-        if(other.gameObject.tag == tag )
+        
+        if(other.gameObject.tag.Contains("Object_")) // checks if the other.gameobject.tag have Object_ .
         {
-            player_score++;
+            SetPlayerScore(other.gameObject.tag);
+        }
+    }
+
+    void SetPlayerScore(string ObjectTag)
+    {
+        switch(ObjectTag)
+        {
+            case "Object_Yellow": // needs to change to Object_<what the object is> example: Object_Wallet.
+                _player_score += 1;
+                break;
+            case "Object_Red": // needs to change to Object_<what the object is> example: Object_Wallet.
+                _player_score += 2;
+                break;
+            case "Object_Blue": // needs to change to Object_<what the object is> example: Object_Wallet.
+                _player_score += 3;
+                break;
         }
     }
 }
