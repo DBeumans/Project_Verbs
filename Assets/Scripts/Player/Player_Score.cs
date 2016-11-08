0d44b4b0b5fbd1603b4680fb545cdf26bbd4744e
lucasmuileboom <muileboomlucas@gmail.com>
2016-11-08 09:21:44 +0100

    added highscore

--- Assets/Scripts/Player/Player_Score.cs
+++ Assets/Scripts/Player/Player_Score.cs
@@ -3,6 +3,42 @@ using System.Collections;
 
 public class Player_Score : MonoBehaviour {
 
-   public int player_score = 0; 
+    int _player_score; // private variable, do not change this to public, use the other player score variable to use it in other scripts.
+	public highscore highScore;
 
+    // use PlayerScore to use it for other scripts , example UI, save scripts.
+    public int PlayerScore // Read only.
+    {
+        get
+        {
+            return _player_score;
+        }
+    }
+
+	void Start()
+	{
+		highScore = GetComponent<highscore>();
+	}
+
+    public void SetPlayerScore(string ObjectTag)
+    {
+        switch(ObjectTag)
+        {
+		case "Object_Yellow": // needs to change to Object_<what the object is> example: Object_Wallet.
+				_player_score += 1;
+				highScore.HighScore ();
+
+                break;
+            case "Object_Red": // needs to change to Object_<what the object is> example: Object_Wallet.
+                _player_score += 2;
+				highScore.HighScore ();
+                
+                break;
+            case "Object_Blue": // needs to change to Object_<what the object is> example: Object_Wallet.
+                _player_score += 3;
+				highScore.HighScore ();
+                
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/highscore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class highscore : MonoBehaviour 
+{
+	int highScore;
+	public Player_Score Score;
+
+	void Start()
+	{
+		Score = GetComponent<Player_Score>();
+	}
+
+	public void HighScore ()
+	{
+		if (highScore < Score.PlayerScore)
+		{
+			highScore = Score.PlayerScore;
+		}
+	}
+}
