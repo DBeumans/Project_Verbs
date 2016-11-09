d3bd67e00266873ed0400b116ae3742989519edd
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-09 23:37:31 +0100

    edited the switch_scene script

--- Assets/Scripts/UI/Switch_Scene.cs
+++ Assets/Scripts/UI/Switch_Scene.cs
@@ -11,17 +11,6 @@ public class Switch_Scene : MonoBehaviour {
 
     public void LoadScene(int index)
     {
-        switch (index)
-        {
-            case 0: // main menu
-                SceneManager.LoadScene(0);
-                break;
-            case 1: // game scene
-                SceneManager.LoadScene(1);
-                break;
-            case 2: // game over scene
-                SceneManager.LoadScene(2);
-                break;
-        }
+        SceneManager.LoadScene(index);
     }
 }
